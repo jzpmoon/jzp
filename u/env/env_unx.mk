5a13eff74407349ecede5fe7137217bc1ee758c9
zp j <2481957497@qq.com>
2022-03-23 21:43:23 +0800

    20220323

--- u/env/env_unx.mk
+++ u/env/env_unx.mk
@@ -1,3 +1,7 @@
 suf_pg :=
 suf_so := .so
 suf_lb := .a
+CC := gcc
+LINK := ld
+out := -o
+c := -c
--- u/env/env_win.mk
+++ u/env/env_win.mk
@@ -1,3 +1,7 @@
 suf_pg := .exe
 suf_so := .dll
 suf_lb := .lib
+CC := cl.exe
+LINK := link.exe
+out := /out
+c := /c
